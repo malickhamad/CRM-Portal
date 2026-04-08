05f1e2209864302c21001deedb72bbf1b990dc2f
AdrashUmair-web <adrashumair01@gmail.com>
2026-04-09 00:31:08 +0500

    application smain page aded

--- routes/admin.php
+++ routes/admin.php
@@ -33,6 +33,7 @@ Route::middleware(['auth', 'role:Admin'])->prefix('admin')->name('admin.')->grou
  Route::get('/services', [ApplicationController::class, 'services'])->name('services');
  Route::get('/finance_services', [ApplicationController::class, 'finance_services'])->name('finance_services');
  Route::get('/utilities_services', [ApplicationController::class, 'utilities_services'])->name('utilities_services');
+ Route::get('/applications', [ApplicationController::class, 'applications'])->name('applications');
 
 
 Route::prefix('finance')->group(function () {
